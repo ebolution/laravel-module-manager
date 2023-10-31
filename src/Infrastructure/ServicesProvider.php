87177f6988989fb35ced238441ad41a717de1c52
Carlos Cid <carlos.cid@ebolution.com>
2023-10-31 18:22:05 +0100

    feat: Allows to publish module test cases

--- src/Infrastructure/ServicesProvider.php
+++ src/Infrastructure/ServicesProvider.php
@@ -35,4 +35,37 @@ class ServicesProvider extends AggregateServiceProvider
             $artisan->resolveCommands($commands);
         });
     }
+
+    public function publishTests(string $testPath, string $moduleFolder): void
+    {
+        $tests = $this->discoverTests($testPath, $moduleFolder);
+        $this->publishes($tests, 'laravel-assets');
+    }
+
+    private function discoverTests(string $testPath, string $moduleFolder): array
+    {
+        $tests = [];
+        $absolute_test_path = realpath($testPath);
+        $test_path_length = strlen($absolute_test_path);
+        $last_folder_length = strlen(basename($absolute_test_path));
+
+        $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($absolute_test_path));
+        $files = array_reverse(iterator_to_array($iterator));
+
+        foreach ($files as $file) {
+            $real_path = $file->getRealPath();
+            if ($file->isFile() and $file->getExtension() === 'php') {
+                $relative_file_path = substr($real_path, $test_path_length - $last_folder_length);
+                $test_file_name = basename($relative_file_path);
+                $test_file_folder = dirname($relative_file_path);
+                $tests[$real_path] = base_path(
+                    $test_file_folder . DIRECTORY_SEPARATOR .
+                    $moduleFolder . DIRECTORY_SEPARATOR .
+                    $test_file_name
+                );
+            }
+        }
+
+        return $tests;
+    }
 }
